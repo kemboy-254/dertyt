48607a528eaa4e1a46982a1f34bdf583341e4cb2
CodexRodney <rodney002mwanje@gmail.com>
2021-11-04 00:00:53 +0300

    Rhrhjjs

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -5,36 +5,36 @@
 #include <sys/wait.h>
 
 /**
- * main- It runs a simple shell that prompts the user to 
+ * main- It runs a simple shell that prompts the user to
  *	a command and then it runs the command or display
  *	the error if command fails
  *
  * Return: Always 0
  */
 
-int main()
+int main(void)
 {
 	char *buf = "#cisfun $ ";
 	size_t size = 100;
 	char *string = NULL;
-	const char* const argv[] = {NULL};
-	const char* const envp[] = {NULL};
+	const* char const argv[] = {NULL};
+	const* char const envp[] = {NULL};
 	const char *cmd;
 	pid_t pid;
 
-	while(1)
+	while (1)
 	{
-		write(1,buf,strlen(buf));
-		if(getline(&string,&size,stdin) == -1)
+		write(1, buf, strlen(buf));
+		if (getline(&string, &size, stdin) == -1)
 		{
-			if(feof(stdin))
+			if (feof(stdin))
 			{
 				exit(EXIT_SUCCESS);
 			}
 			else
 			{
 				perror(".hsh ");
-				exit (EXIT_FAILURE);
+				exit(EXIT_FAILURE);
 			}
 		}
 		cmd = string;
@@ -45,16 +45,16 @@ int main()
 		}
 		else if (pid == 0)
 		{
-			if (execve(cmd,argv,envp) == -1)
+			if (execve(cmd, argv, envp) == -1)
 			{
 				perror(".hsh");
 			}
-			exit (0);
+			exit(0);
 		}
 		else
 		{
 			wait();
 		}
 	}
-	return 0;
+	return (0);
 }
